74baa367493824a1a151adbcec48ae98a5c81008
Mohamed hany <mohammedhany4213@gmail.com>
2026-09-09 19:29:54 +0300

    add User entity class and configurations

---
+++ src/SimpleAutomationEngine.Domain/Entities/User.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations ;
+
+namespace SimpleAutomationEngine.Domain.Entities
+{
+    public class User
+    {
+        [Key]
+        public int UserId {get; set;}
+
+        [Required]
+        [MaxLength(100)]
+        public string FullName {get; set;} = string.Empty ;
+
+        [Required]
+        [EmailAddress]
+        public string Email {get; set;} = string.Empty ;
+
+        [Required]
+        public string PasswordHash {get; set;}  = string.Empty ;
+
+        public DateTime CreatedAt {get; set;} = DateTime.UtcNow ;
+
+        public bool IsActive {get; set;} = true ;
+
+        public string? RefreshToken {get; set;}  
+        public DateTime? RefreshTokenExpirationTime {get; set;}  
+
+        public ICollection<Action> Actions {get; set;} = new List<Action>();
+    }
+} 
